0479a4390e5a68acfccd20aae8af07eb9f9ba78e
Ahmed <Ahmed3679117@gmail.com>
2026-04-30 17:53:09 +0300

    feat(core): add Exam and ExamResult tracking entities

---
+++ NetSupport-Core/Entites/Exam.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetSupport_MVP_Project.Models
+{
+    public class Exam
+    {
+        public int Id { get; set; }
+        [Range(1,300)]
+        public double DurationInMinutes { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Title { get; set; }
+        public virtual ICollection<Question> Questions { get; set; } = new HashSet<Question>();
+        public virtual ICollection<ExamResult> ExamResults { get; set; } = new HashSet<ExamResult>();
+
+    }
+}
---
+++ NetSupport-Core/Entites/ExamResult.cs
@@ -0,0 +1,14 @@
+﻿namespace NetSupport_MVP_Project.Models
+{
+    public class ExamResult
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int ExamId { get; set; }
+        public int Score { get; set; }
+        public int TotalQuestions { get; set; }
+        public virtual Exam Exam { get; set; }
+        public virtual Student Student { get; set; }
+        
+    }
+}
